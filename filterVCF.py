19624c687dcddbc61d59e2c51b484cc4dc6e72ed
lorenzo <dm.lorenzo@libero.it>
2023-12-12 10:55:24 +0100

    Update of Pathopole
    * config file
    * log system
    * different input format
    * requirements for installation

--- filterVCF.py
+++ filterVCF.py
@@ -1,12 +1,12 @@
-#funzione per filtrare le colonne ALT e FILTER di un file vcf e creare un nuovo
-#vcf filtrato
-
 import argparse
 import pandas as pd
 import vcf
+import os
+from configparser import ConfigParser
+from loguru import logger
+
 
 def parsing_vcf(file_input,file_output):
-    
     #lista contenente i dati da #CHROM in poi (no meta-information)
     with open (file_input) as f:
         correct_data=[line.strip().split("\t") for line in f if not line.startswith("##")]
@@ -14,17 +14,19 @@ def parsing_vcf(file_input,file_output):
     #creazione dataframe contenente i dati
     data=pd.DataFrame(correct_data[1:],columns=correct_data[0])
     
-    #filtro per dati in cui ALT!="."
-    data=data[data["ALT"]!="."]
-
     #filtro per dati in cui FILTER == "PASS"
     data=data[data["FILTER"]=="PASS"]
+    
+    #filtro per dati in cui ALT!="."
+    data=data[data["ALT"]!="."]
+    
     # print(type(data))
     #per salvare il file così filtrato
     data.to_csv(file_output,sep="\t", mode="a", index=False)
 
 
 def write_vcf_meta(file_path, metadata):
+    
     # input file path output - output write meta data file
     with open(file_path, 'w') as meta_file:
             for key, value in metadata.items():
@@ -43,93 +45,25 @@ def read_vcf_meta(file_path):
     
     
 
-def main(INPUT, OUTPUT):
-    metadata = read_vcf_meta(INPUT)
+def main_filter(input, output):
 
-    write_vcf_meta(OUTPUT, metadata)
-    parsing_vcf(INPUT,OUTPUT)
-
-if __name__ == '__main__':
-
-
-# parse arguments
-    parser = argparse.ArgumentParser(description="Filter for a vcf",
-                                            epilog="Version: 1.0\n\
-                                            Author: Bioinformatics Facility GSTeP'\n\
-                                            email: luciano.giaco@policlinicogemelli.it")
-
-    # arguments
-    parser.add_argument('-i', '--input', help="<input.vcf>\
-                                            VCF file to filter",
-                                            required=True)
-    parser.add_argument('-o', '--output', help="<output-file.tab>\
-                                            file path of the Table output",
-                                            required=True)
-
-    args = parser.parse_args()
-    INPUT = args.input
-    OUTPUT = args.output
-
-    main(INPUT, OUTPUT)
-
-
-
-
-''' possibili ulteriori funzioni
-#ecco una possibile nuova funzione che prende come input una delle chiavi del dizionario (quindi una mutazione) e ti ritorna da solo il valore associato (quindi il numero di mutazioni osservate)
-def select(vcf):
-    vcf=dizionario(vcf)
-    print ("Scegli la mutazione da vedere:")
-    richiesta= input()
-    for key, value in vcf.items():
-        if key == str(richiesta):
-            print('The amount of', str(richiesta), 'is: ', value)#per i valori presenti nel dizionario, ti ritorna il valore corrispendete alla chiava data come input
-            
-#in caso parlane con Luciano potrebbe essere figa da mettere
-
-#------------------------------
-
-
-#possibile nuova funzione per comparare 2 dizionari magari presi da 2 vcf
-# basato sulla richiesta del dottor. Angelo Minucci che ha chiesto se è possibile trovarne le differenze tra 2 file di input
-
-def compare(file1, file2):
-    file1=dizionario(file1)
-    file2 = dizionario(file2)
-    valori_uguali = {k: file1[k] for k in file1 if k in file2 and file1[k] == file2[k]}
-    print(len(valori_uguali))
-#finora questo da i valori uguali presenti nei 2 vcf, sia per le key che per i values. a noi frega poco se sono uguali, dato che è poco probabile che accade una cosa simile.
-    valori_differenti = {k: file1[k] for k in file1 if k in file2 and file1[k] != file2[k]}
-    print(len(valori_differenti))
-
-
-
-#TMB(Tumor mutational burden)= The total number of mutations (changes) found in the DNA of cancer cells
-#per trovarlo devi fare le mutazioni sul totale
-#TMB > 100 mut/Mb.
-#nell'articolo "we used 38 Mb as the estimate of the exome size"
-#For comparison, 321 microsatellite-stable (MSS)
-
-all 321 MSS, POLE wild-type ECs scored ≤3 points and all 127 MSI-H POLE wild-type ECs scored ≤2.
-COSMIC signature 10 was identified in 11 (8.7%) MSI-POLEwt and 96 (29.9%) MSS-POLEwt ECs (mean signature 10 contribution 0.002, range 0.000–0.048, and mean 0.017, range 0.000–0.218, respectively).
-
-
-aldilà dei microsatelliti, l'articolo parla di "recurrent variant in EC" quindi chiede se ci sono immagino delle mutazioni più frequenti rispetto ad altre
-quindi, MAGARI, lo score deve aumentare di +1 se per ipotesi una dei tipi di mutazioni super un certo numero nello specifico
-si ma quale. controlla sull'articolo
-
-def findsatellites(file):
-    lista_ALT=[] #i microsatelliti sono visibili nel ALT come quei valori che sono sequenze tipo AATTCCGG e simili
-    import vcf
-    vcf_reader = vcf.Reader(open(file, 'r')) #leggi il vcf di input
-    for record in vcf_reader:
-            lista_ALT.append(str(record.ALT)) #mettimi nella lista tutti i valori ALT
-            
-    lunghezza_minima_microsatelliti = 4
-    #lunghezza minima presente nei dati di ALT per trovare i valori dei microsatelliti
-    #qui sotto la funzione inserisce in una nuova lista tutte le ALT che non hanno la lunghezza data
-    lista_microsatelliti=[]
-    lista_microsatelliti=([x for x in lista_REF if len(x) >= lunghezza_minima_microsatelliti])
     
+    if not os.path.exists(output):
+        logger.warning("Creating folder to store filtered vcf")
+        os.makedirs(output)
+    elif not os.path.isdir(output):
+        print("Output is not a folder")
+        exit(1)
+    
+    metadata = read_vcf_meta(input)
+  
+    filtered_path=os.path.join(output,input.split("/")[-1]+"_filtered.vcf").replace(".vcf_","_")    
+    
+    write_vcf_meta(filtered_path, metadata)
+    parsing_vcf(input,filtered_path)
 
-'''
+
+
+
+
+
--- pathopoleanalyzer.py
+++ pathopoleanalyzer.py
@@ -1,41 +1,35 @@
+######################################
+# NAME: pathopoleanalyzer.py
+# version ="1.0"
+# ====================================
+
+import os
+import sys
 import vcf
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import os 
+import argparse
+from loguru import logger
+from configparser import ConfigParser
+from filterVCF import main_filter
+
+config = ConfigParser()
+configFile = config.read("conf.ini")
+
+
 
 def dicto(file):
-
-    mutations= ('A>C', 'A>G', 'A>T', 'C>A', 'C>G', 'C>T', 'G>A', 'G>C', 'G>T', 'T>A', 'T>C', 'T>G')
+    mutations= ('C>A', 'C>G','T>G')
     dictionary=dict.fromkeys(mutations)
     number=0
     dictionary=dict.fromkeys(mutations,number)
     vcf_reader = vcf.Reader(open(file, 'r'))
     for record in vcf_reader:
         for alt in range(0,len(record.ALT)):
-        
-            if record.REF == 'A' and record.ALT[alt]=='C':
-                dictionary['A>C']+=1
-            if record.REF == 'A' and record.ALT[alt]=='G':
-                dictionary['A>G']+=1
-            if record.REF == 'A' and record.ALT[alt]=='T':
-                dictionary['A>T']+=1    
             if record.REF == 'C' and record.ALT[alt]=='A':
                 dictionary['C>A']+=1
             if record.REF == 'C' and record.ALT[alt]=='G':
                 dictionary['C>G']+=1
-            if record.REF == 'C' and record.ALT[alt]=='T':
-                dictionary['C>T']+=1
-            if record.REF == 'G' and record.ALT[alt]==['A']:
-                dictionary['G>A']+=1
-            if record.REF == 'G' and record.ALT[alt]=='C':
-                dictionary['G>C']+=1
-            if record.REF == 'G' and record.ALT[alt]=='T':
-                dictionary['G>T']+=1      
-            if record.REF == 'T' and record.ALT[alt]=='A':
-                dictionary['T>A']+=1
-            if record.REF == 'T' and record.ALT[alt]=='C':
-                dictionary['T>C']+=1
             if record.REF == 'T' and record.ALT[alt]=='G':
                 dictionary['T>G']+=1
     return dictionary
@@ -52,21 +46,38 @@ def recurrentmutations(file):
                 y= str(record.REF)+">"+str(record.ALT[alt])
                 totalmuts[x] = y
 
-    listrecurrent = {133253184: 'G>C', 133250289: 'C>A', 133253151: 'G>A', 133249847: 'G>A', 133249857: 'C>G', 133252327: 'A>G', 133250250: 'G>T', 133253157: 'A>C', 133250213: 'G>C', 133250189: 'A>T', 133252325: 'C>A', 133250250: 'G>C', 133250238: 'C>T', 133250250: 'G>C', 133253208: 'G>A', 133249829: 'G>A', 133256623: 'G>A', 133252023: 'T>G', 133257828: 'G>A'}
-
+    listrecurrent = {133253184: 'G>C',
+                     133250289: 'C>A', 
+                     133253151: 'G>A', 
+                     133249847: 'G>A',
+                     133249857: 'C>G',
+                     133252327: 'A>G', 
+                     133250250: 'G>T',
+                     133253157: 'A>C',
+                     133250213: 'G>C',
+                     133250189: 'A>T', 
+                     133252325: 'C>A', 
+                     133250250: 'G>C', 
+                     133250238: 'C>T', 
+                     133250250: 'G>C',
+                     133253208: 'G>A',
+                     133249829: 'G>A', 
+                     133256623: 'G>A',
+                     133252023: 'T>G',
+                     133257828: 'G>A'}
     
     shared_items = {k: listrecurrent[k] for k in listrecurrent if k in totalmuts and listrecurrent[k] == totalmuts[k]}
     if len(shared_items) == 0:
-        return shared_items   
+        return "None"   
     else:
-        return shared_items
+        return True
 
 
 def list_indels(file):
     indels_list=[]
     lista_inserzioni=[]
     lista_delezioni=[]
-    vcf_reader = vcf.Reader(open(file, 'r')) #leggi il vcf di input
+    vcf_reader = vcf.Reader(open(file, 'r')) 
     for record in vcf_reader:
         for alt in range(0,len(record.ALT)):
             if len(record.ALT[alt])!=len(record.REF):
@@ -88,7 +99,6 @@ def totalmutationevents(file):
     return mutation_totals
 
 
-
 def mutationsfrequency(file):
     dict = dicto(file)
     totalsos = sum(dict.values())
@@ -98,30 +108,26 @@ def mutationsfrequency(file):
         listpercent.append(percent)
     return ' '
 
-
-
-
 def polescore(file, TMB):
     score=0
     dictionary = dicto(file)
     indels_list= list_indels(file)
     mutation_totals = totalmutationevents(file)
     totalsos = sum(dictionary.values())
-
-    if float(TMB)>= 100: 
+    if float(TMB)>= float(config.get('Threshold', 'TMB')): 
         score +=1
     for key, value in dictionary.items():
 
         if key=="C>A":
-            if round((int(value) / totalsos),4)*100 >= 6:
+            if round((int(value) / totalsos),4)*100 >= float(config.get('Threshold', 'C_A')):
                 score +=1
                
         if key=="T>G": 
-            if round((int(value) / totalsos),4)*100 >= 4:
+            if round((int(value) / totalsos),4)*100 >= float(config.get('Threshold', 'T_G')):
                 score +=1
             
         if key=="C>G":
-            if round((int(value) / totalsos),4)*100 <= 5:
+            if round((int(value) / totalsos),4)*100 <= float(config.get('Threshold', 'C_G')):
                 score +=1
 
     
@@ -130,71 +136,131 @@ def polescore(file, TMB):
     t_g=round(dictionary["T>G"]/totalsos,2)*100
 
     ratio_indels=round((len(indels_list)/ mutation_totals),4) * 100
-    if ratio_indels <= 4:
+    if ratio_indels <= float(config.get('Threshold', 'Indels')):
         score+=1
 
     response="Recurrent Mutations not found"
     recurrent = recurrentmutations(file)    
-    if len(recurrent) != 0:
+    if recurrent != "None":
         response = "+    Recurrent Mutations in EC"
         score+=1
-    
     if score >= 4:
         output = 'Pathogenic POLE mutation'
     if score == 3:
         output = 'Variant of unknown significance'
     if score < 3:
         output= 'Non-Pathogenic POLE mutation'
-    return score,TMB,recurrent,ratio_indels,c_a,c_g,t_g
+    return score,TMB,recurrent,round(ratio_indels,2),round(c_a,2),round(c_g,2),round(t_g,2)
 
 
-import argparse
 
 if __name__ == '__main__':
     
+    logfile="PathoPOLE_{time:YYYY-MM-DD_HH-mm-ss.SS}.log"
+    logger.level("INFO", color="<green>")
+   # logger.add(sys.stderr, format="{time:YYYY-MM-DD_HH-mm-ss.SS} | <lvl>{level} </lvl>| {message}",colorize=True, catch=True, backtrace=True, diagnose=True)
+    logger.add(os.path.join('Logs',logfile),format="{time:YYYY-MM-DD_HH-mm-ss.SS} | <lvl>{level} </lvl>| {message}",mode="w", backtrace=True, diagnose=True)
+    logger.info("Welcome to Patho POLE Analyzer")
+
     parser = argparse.ArgumentParser(
                     prog = 'VCF scoring system for POLE pathogenicity', 
                     description= 'Script which generates a set of objects that takes in account the mutation events in an input filtered VCF, to thene generate a score which estimates the patogenic nature of the POLE mutation.', 
                     epilog = 'The generated score is a numeric integer which is derived by the count of specific conditions correlated to the pathogenicity of POLE mutations: during a vcf analysis, if a pathogenicity threshold is met, the score will go up by +1. The final score is given by the sum of all the respected thresholds',
                     add_help = True, )
-        # arguments
-    parser.add_argument('-f', '--folder', required=True, help='Path to the VCF folder')
-    parser.add_argument('-t', '--TMB', required=True, help='Tumour Mutational Burden (TMB)')
+    parser.add_argument('-i', '--Input', required=True, help='Path to the VCF folder')
+    parser.add_argument('-t', '--TMB', required=True, help='Tumour Mutational Burden (TMB) or file')
     parser.add_argument('-o', '--output', required=False, help='Path of file to store results')
+    parser.add_argument('-w', '--overWrite', required=False, action='store_true',
+                        help='Overwrite result file')
     
-    args = parser.parse_args()
+    try:
+        args = parser.parse_args()
+    except ValueError:
+        logger.critical("Error Argument: missing required input")
+        exit(1)
+    
 
-    folder = args.folder
-    TMB = args.TMB
-    output=args.output
+    filter_output=config.get("Filter","path_filtered_vcf")
     
-    print("\n")
-    print("Analyzed VCF: ", folder, "\n")
+    input=args.Input
 
-    
-    print('--------------------------', "\n")
-    
-    
-    print('##########################', "\n") 
-    score_tso,TMB,recurrent,ratio_indels,c_a,c_g,t_g=polescore(folder, TMB)
-    print('Score Pathogenicity = ', polescore(folder, TMB),'\n')
-    
-    
-    if args.output: 
-        if os.path.exists(output):
-            file=open(output,"a")
-        else:
-            file=open(output,"w")
-            file.write("\t".join(["SampleID","TMB","RecurrentMutation","Indels","C>A","C>G","T>G","Score"])+"\n")
+    if args.output:
+            output=args.output
+    else:
+        logger.warning("Using default output file 'Results_pathopoleanalyzer.txt'")
+        output=config.get("Results","results_file")
         
-        sample_id=folder.split("/")[-1]
 
-        file.write("\t".join([str(sample_id),str(TMB).strip(),str(recurrent), str(ratio_indels),str(c_a),str(c_g),str(t_g),str(score_tso)])+"\n")
+
+    if os.path.isfile(input):
+
+        if not input.endswith(".vcf"):
+            logger.critical("Input file is not a VCF")
+            exit(1)
+
+        main_filter(input,filter_output)
+        TMB = args.TMB
+        
+      
+        logger.info("Analyzing VCF "+input)
+        score_tso,TMB,recurrent,ratio_indels,c_a,c_g,t_g=polescore(os.path.join(filter_output,input.split("/")[-1]+"_filtered.vcf").replace(".vcf_","_"),TMB)
+        logger.info('Score Pathogenicity = +'+str(score_tso))
+    
+        if config.get("Results","store_results"): 
+            if os.path.exists(output):
+                file=open(output,"a")
+            else:
+                file=open(output,"w")
+                file.write("\t".join(["SampleID","TMB","RecurrentMutation","Indels","C>A","C>G","T>G","Score"])+"\n")
             
+            sample_id=input.split("/")[-1]
+
+            file.write("\t".join([str(sample_id),str(TMB).strip(),str(recurrent), str(ratio_indels),str(c_a),str(c_g),str(t_g),str(score_tso)])+"\n")
+
+        logger.success("VCF analysis completed successfully!")
+
+    elif os.path.isdir(args.Input):
+        logger.warning("Input is a folder. Reading multiple files")
+        
+        files=[file for file in os.listdir(args.Input) if ".vcf" in file]
+        if len(files)==0:
+            logger.critical("No vcf found in input folder")
+            exit(1)
+
+        logger.info("Found "+str(len(files))+" vcf files")
+
+        for file in files:
+            main_filter(os.path.join(input,file),filter_output)
+             
+
+        if not os.path.isfile(args.TMB):
+            logger.critical("Error: TMB must be a file")
+            exit()
+        else:
+            tmb=pd.read_csv(args.TMB,sep="\t")
+            
+            results=pd.DataFrame(columns=["SampleID","TMB","RecurrentMutation","Indels","C>A","C>G","T>G","Score"])
+            for file in files:
+
+                tmb_sample=tmb.loc[tmb["Sample"]==file,"TMB"].values[0]
+           
+               
+                logger.info("Analyzing VCF "+file)
+                score_tso,tmb_sample,recurrent,ratio_indels,c_a,c_g,t_g=polescore(os.path.join(filter_output,file.split("/")[-1]+"_filtered.vcf").replace(".vcf_","_"),tmb_sample)
+                logger.info('Score Pathogenicity of '+file +': '+str(score_tso))
+
+                sample_id=file.split("/")[-1]
+                temp=pd.DataFrame({"SampleID":str(sample_id),"TMB":tmb_sample,"RecurrentMutation":recurrent,"Indels":ratio_indels,
+                                   "C>A":c_a,"C>G":c_g,"T>G":t_g,"Score":score_tso},index=[0])
 
 
+                results=pd.concat([results,temp],axis=0).reset_index(drop=True)
 
-
-
-
-
+            if config.get("Results","store_results"): 
+                logger.info("Writing results in output file")
+                if os.path.exists(output) and not args.overWrite:
+                   results.to_csv(output, sep="\t", mode="a",header=False,index=False)
+                else:
+                    results.to_csv(output, sep="\t", mode="w",index=False)
+                    
+            logger.success("VCF analysis completed successfully!")
