a3f3ec66cbc958324a5f5c195f660cc736938813
Luciano Giaco <91454270+lucianogiaco@users.noreply.github.com>
2024-05-17 14:48:00 +0200

    fixed for vcf v4.2

--- filterVCF.py
+++ filterVCF.py
@@ -33,7 +33,10 @@ def write_vcf_meta(file_path, metadata):
                 if type(value) is str:
                     meta_file.write('##'+ key + ': ' + value + '\n')
                 elif type(value) is list:
-                    meta_file.write('##'+ key + ': '.join(value) + '\n')
+                    for x in value:
+                        print('type')
+                        print(type(value))
+                        meta_file.write('##'+ key + ': '.join(x) + '\n')
 
 
 def read_vcf_meta(file_path):
